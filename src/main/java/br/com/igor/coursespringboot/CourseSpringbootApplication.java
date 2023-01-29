8fba2b34edd6cfc1195c74fe52e9abb20875db12
igorferreira007 <ferreiraigor007@gmail.com>
2023-01-29 19:27:57 -0300

    Projeto criado

---
+++ src/main/java/br/com/igor/coursespringboot/CourseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.igor.coursespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igor/coursespringboot/CourseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igor.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
